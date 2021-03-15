6bc3704623e7e3bde60ac01291a769e41f62a22c
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-15 17:09:57 +0500

    python input

---
+++ input.py
@@ -0,0 +1,12 @@
+## taking user input with 'input()' function
+
+name = input("What is your name? ")
+print("Hi "+ name)
+
+
+# Exercise
+## Ask two questions: person's name and favourite color, and print the message like Fahad Likes Green
+
+full_name = input("What is your full name? ")
+color = input("What's your favourite color? ")
+print(name+" Likes "+color)
