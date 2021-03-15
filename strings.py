05bb1303d3028e89daad5f2c2084dc71e9f1184a
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-15 17:09:45 +0500

    python strings

---
+++ strings.py
@@ -0,0 +1,24 @@
+single_qoute_strings = 'this is fahad'
+print(single_qoute_strings)
+
+double_qoute_strings = "This is fahad and I am learning from Mosh's python course!"
+print(double_qoute_strings)
+
+triple_qoute_strings = ''' 
+
+Welcome to python programming course, 
+
+you are working on triple qoute strins
+
+you did great!
+
+'''
+
+print(triple_qoute_strings)
+
+## formatted string
+
+first_name = "fahad"
+last_name = "aleem"
+full_name = f'{first_name} {last_name}'
+print(full_name)
