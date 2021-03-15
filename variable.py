3e8dfbdec219c3aea95e92670c6846c0e29b131f
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-15 17:08:56 +0500

    python variables

---
+++ variable.py
@@ -0,0 +1,23 @@
+#integer
+price =10
+
+# float
+rating = 2.3
+
+# string
+name = "Fahad Aleem"
+
+# boolean
+isLearned = False
+
+
+## Exerice question 
+## We check in a patient named john smith, He's 20 years old and is to check a new patient?
+
+full_name = "John Smith"
+age = 20
+is_new = False
+
+print(full_name)
+print(age)
+print(is_new)
