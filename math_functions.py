0ac41140a18f0584127b4f982cbeef506c5ba6d9
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-22 00:24:30 +0500

    Math Functions

---
+++ math_functions.py
@@ -0,0 +1,19 @@
+import math
+
+# power
+print(f"power Function: math.pow(2,3) = {math.pow(2,3)}")
+
+# ceil
+print(f"ceil Function: math.ceil(2.3) = {math.ceil(2.3)}")
+
+# Floor
+print(f"floor Function: math.ceil(2.3) = {math.floor(2.3)}")
+
+# radian
+print(f"radian Function: math.ceil(25) = {math.radians(25)}")
+
+# degree
+print(f"degree Function: math.degree(0.43) = {math.degrees(0.43)}")
+
+# factorial
+print(f"factorial Function: math.factorial(5) = {math.factorial(5)}")
