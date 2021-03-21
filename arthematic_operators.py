58c88c05c7978e70e79aa7477ef70089f28b3ad4
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-22 00:24:12 +0500

    Arthematic Operators of python

---
+++ arthematic_operators.py
@@ -0,0 +1,26 @@
+## Arthematic Operators
+## addition
+print(f"Addition of {4} and {3} is: {4+3}")
+
+## subtraction
+print(f"Subtraction of {4} and {3} is: {4-3}")
+
+## multiplication
+print(f"Multiplication of {4} and {3} is: {4*3}")
+
+## division
+print(f"Division of {4} and {3} is: {4/3}")
+
+## double division
+print(f"Double Division of {4} and {3} is: {4//3}")
+
+## mod
+print(f"Mod of {4} and {3} is: {4%3}")
+
+## power
+print(f"Power of {4} is {3} is: {4**3}")
+
+## augmented operator
+x = 3
+x+=3
+print(f"the value of x: {x}")
