143800fdd91277e6581434fc0255808235b9359a
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-23 10:12:40 +0500

    Lists

---
+++ lists.py
@@ -0,0 +1,11 @@
+## list in python, 
+
+names = ["Fahad", "Asad", "Ammar", "Nameer","Zaid"]
+print(names)
+print(names[0])
+print(names[:])
+print(names[0:2])
+print(names[1:-2])
+print(names[1:])
+
+
