3e4b739c217fb4334f33b7fbaf482eac9977b7db
unknown <55811140+fahadaleem@users.noreply.github.com>
2021-03-23 09:58:36 +0500

    for loop syntax

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+## for loop in python
+
+for item in "strings":
+    print(item)
+
+
+print("to iterate on an array items")
+for item in ["Banana", "Apple", "Strawberry"]:
+    print(item)    
+
+
+## range function to apply limts
+## 0-4
+## 0, 1,2,3,4
+print("for loop to limit the numbers")
+for item in range(5):
+    print(item)
+
+## to set the starting number
+print("for loop to set the starting number ")
+for item in range(1,9):
+    print(item)
+
+## to set the step:
+print("for Loop with Step in range:")
+for item in range(1,10, 2):
+    print(item)
